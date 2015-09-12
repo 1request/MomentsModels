e8bf284d630a36e240365aea29e917516a11abb2
Harry Ng <harryworld@gmail.com>
2015-09-13 00:04:59 +0800

    Update code based on Realm 0.95

--- Pod/Classes/BBRealmMigrationManager.swift
+++ Pod/Classes/BBRealmMigrationManager.swift
@@ -12,12 +12,16 @@ import RealmSwift
 public class BBRealmMigrationManager {
    
     public class func migrate() {
-        setSchemaVersion(1, Realm.defaultPath) { (migration, oldSchemaVersion) in
+        let config = Realm.Configuration(
+            schemaVersion: 1,
             
-            if oldSchemaVersion < 1 {
-                // Video Migration
+            migrationBlock: { migration, oldSchemaVersion in
+                if (oldSchemaVersion < 1) {
+                    
+                }
             }
-        }
+        )
+        Realm.Configuration.defaultConfiguration = config
     }
     
 }
